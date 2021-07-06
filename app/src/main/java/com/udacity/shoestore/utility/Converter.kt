808a04acc41c44d1a403d87b61a5fe90b3dc1e5f
Siddharth Patel <spatel@igloosoftware.com>
2021-07-06 18:00:37 -0400

    Trying to implement two-way data binding for shoe size decimal value, but still not successful

--- app/src/main/java/com/udacity/shoestore/utility/Converter.kt
+++ app/src/main/java/com/udacity/shoestore/utility/Converter.kt
@@ -1,21 +1,32 @@
 package com.udacity.shoestore.utility
 
+import android.widget.TextView
 import androidx.databinding.InverseMethod
 import timber.log.Timber
+import java.text.ParseException
 
-class Converter {
-
-        fun toText(shoeSize: Double): String {
-            return try {
-                shoeSize.toString()
-            } catch (ex: Exception) {
-                Timber.e("Something wrong with Shoe Size Value")
-                ""
+object Converter {
+       @InverseMethod("textToDecimal")
+        fun decimalToText(view: TextView, shoeOldSize: Double, shoeSize: Double): String {
+            try {
+                val editTextValue = view.text.toString()
+                val parsedValue = editTextValue.toDouble()
+                if (parsedValue == shoeSize)
+                    return view.text.toString()
             }
+            catch (ex: ParseException) {
+                Timber.e("Something wrong with shoe size number")
+            }
+           return shoeSize.toString()
         }
 
-    @InverseMethod("toText")
-    fun toDecimal(sizeText: String): Double{
-        return sizeText.toDouble()
+    fun textToDecimal(view: TextView, shoeOldSize: Double, shoeSize: String): Double {
+        try {
+            return shoeSize.toDouble()
+        }
+        catch (ex: Exception){
+            Timber.e("Bad shoe size number")
+            return shoeOldSize
+        }
     }
 }
